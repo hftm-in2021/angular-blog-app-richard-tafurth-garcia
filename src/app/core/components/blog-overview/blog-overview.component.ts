b93c919edea78dcc4aacdbe2d6dddbf293c708a5
Richard Tafurth-Garcia <projects@cdstm.ch>
2023-06-08 15:28:28 +0000

    feat(blog): show comments and likes. is somewhat responsive

--- src/app/core/components/blog-overview/blog-overview.component.ts
+++ src/app/core/components/blog-overview/blog-overview.component.ts
@@ -9,12 +9,19 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 import { Blog } from '@app/models/blog';
 
 @Component({
   selector: 'app-blog-overview',
   templateUrl: './blog-overview.component.html',
-  imports: [MatBadgeModule, MatCardModule, MatBadgeModule, MatButtonModule],
+  imports: [
+    MatBadgeModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   styleUrls: ['./blog-overview.component.scss'],
   standalone: true,
 })
