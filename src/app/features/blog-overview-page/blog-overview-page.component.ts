a16eb0ca99e23f28bf54d2f7e5b781ed2b7a78a2
Richard Tafurth-Garcia <projects@cdstm.ch>
2023-06-08 09:19:33 +0000

    feat(blog): created the blog skeletton asked by the teacher

---
+++ src/app/features/blog-overview-page/blog-overview-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-overview-page',
+  templateUrl: './blog-overview-page.component.html',
+  styleUrls: ['./blog-overview-page.component.scss'],
+})
+export class BlogOverviewPageComponent {}
---
+++ src/app/features/blog-overview-page/blog-overview-page.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogOverviewPageComponent } from './blog-overview-page.component';
+
+@NgModule({
+  declarations: [BlogOverviewPageComponent],
+  imports: [CommonModule],
+  exports: [BlogOverviewPageComponent],
+})
+export class BlogOverviewPageModule {}
